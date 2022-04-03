eaa64260fce4a11fea4210abcbc8cee212a2e9c6
Alexandros Adam <alexadam@csd.auth.gr>
2022-04-03 21:09:03 +0300

    pr init

--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,4 +1,21 @@
+import axios from "axios";
 import React, { FC } from "react";
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  const res = axios.get("https://fakestoreapi.com/products");
+
+  return (await res).data;
+};
 
 const Products: FC = () => {
   return <div> Products page.. </div>;
